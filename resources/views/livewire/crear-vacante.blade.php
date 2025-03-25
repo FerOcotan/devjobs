dcf3c4d0e396004e58ccfe524339ebcc225b96fc
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-03-25 17:18:44 -0600

    form vacante

---
+++ resources/views/livewire/crear-vacante.blade.php
@@ -0,0 +1,105 @@
+<form
+
+class="md:w-1/2 space-y-5"
+action="">
+
+<div>
+    <x-input-label for="title" :value="__('Title of vacacie')" />
+    <x-text-input 
+    id="title" 
+    class="block mt-1 w-full"
+     type="text" 
+     name="title" 
+     :value="old('title')" 
+     placeholder="Title of vacacie"
+   />
+    <x-input-error :messages="$errors->get('title')" class="mt-2" />
+</div>
+
+<div>
+    <x-input-label for="salary" :value="__('monthly salary')" />
+    
+    <select 
+    name="salary"
+    id="salary"
+    class=" w-full border-gray-300 focus:border-indigo-500
+     focus:ring-indigo-500 rounded-md shadow-sm">
+
+    </select>
+
+    <x-input-error :messages="$errors->get('salary')" class="mt-2" />
+</div>
+
+<div>
+    <x-input-label for="category" :value="__('category')" />
+    
+    <select 
+    name="category"
+    id="category"
+    class=" w-full border-gray-300 focus:border-indigo-500
+     focus:ring-indigo-500 rounded-md shadow-sm">
+
+    </select>
+
+    <x-input-error :messages="$errors->get('category')" class="mt-2" />
+</div>
+
+<div>
+    <x-input-label for="company" :value="__('company')" />
+    <x-text-input 
+    id="company" 
+    class="block mt-1 w-full"
+     type="text" 
+     name="company" 
+     :value="old('company')" 
+     placeholder="company ej. Netflix, Uber,Samsung"
+   />
+    <x-input-error :messages="$errors->get('company')" class="mt-2" />
+</div>
+
+<div>
+    <x-input-label for="last_day" :value="__('last day to postulate')" />
+    <x-text-input 
+    id="last_day" 
+    class="block mt-1 w-full"
+     type="date" 
+     name="last_day" 
+     :value="old('last_day')" 
+  
+   />
+    <x-input-error :messages="$errors->get('last_day')" class="mt-2" />
+</div>
+
+
+<div>
+    <x-input-label for="description" :value="__('description job')" />
+
+    <textarea
+    name="description"
+    id="description"
+    class="w-full border-gray-300 focus:border-indigo-500
+     focus:ring-indigo-500 rounded-md shadow-sm"
+     rows="5"
+     placeholder="description job"
+    ></textarea>
+
+    <x-input-error :messages="$errors->get('last_day')" class="mt-2" />
+</div>
+
+<div>
+    <x-input-label for="image" :value="__('image')" />
+    <x-text-input 
+    id="image" 
+    class="block mt-1 w-full"
+     type="file" 
+     name="image" 
+     
+   />
+    <x-input-error :messages="$errors->get('title')" class="mt-2" />
+</div>
+
+<x-primary-button>
+    Create Vacante
+</x-primary-button>
+
+</form>
--- resources/views/vacantes/create.blade.php
+++ resources/views/vacantes/create.blade.php
@@ -9,7 +9,15 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    {{ __("Form here") }}
+                      <h1 class="text-3xl font-bold text-center my-10">Publish Vacancie </h1>
+
+                            <div class="md:flex md:justify-center p-5">
+
+                                <livewire:crear-vacante />
+                                
+                            </div>
+
+
                 </div>
             </div>
         </div>
