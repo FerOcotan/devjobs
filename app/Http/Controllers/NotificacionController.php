be3586669894dea549616f82a8d100c119aa87d0
Melvin Fernando Ocotan Morales <melvin.ocotan@catolica.edu.sv>
2025-03-27 11:47:05 -0600

    Mandando solicitudes

--- app/Http/Controllers/NotificacionController.php
+++ app/Http/Controllers/NotificacionController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class NotificacionController extends Controller
 {
@@ -12,6 +13,12 @@ class NotificacionController extends Controller
      */
     public function __invoke(Request $request)
     {
-        dd('notificaciones');
+       
+        $notificaciones = Auth::user()->unreadNotifications;
+
+        return view('notificaciones.index', [
+           'notificaciones' => $notificaciones,
+        ]);
+
     }
 }
---
+++ resources/views/notificaciones/index.blade.php
@@ -0,0 +1,56 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Notificaciones') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                      <h1 class="text-3xl font-bold text-center my-10">Mis Notificaciones </h1>
+
+                            
+
+                                
+                                @forelse ($notificaciones as $notificacion)
+                                <div class="p-5 border-b border-gray-200 lg:flex justify-between lg:items-center">
+                                    <div>
+
+                                        
+                                        <p>Tienes un nuevo candidato en:
+                                            
+                                            <span class="font-bold text-gray-600">
+                                                
+                                                {{ $notificacion->data['nombre_vacante']}}
+                                            </span>
+                                        </p>
+                                        <p>
+                                            <span class="font-bold text-gray-600">
+                                                
+                                                {{ $notificacion->created_at->diffForHumans() }}
+                                            </span>
+                                        </p>
+                                    </div>
+                                    <div class="mt-5 lg:mt-0">
+                                        <a href="" class="bg-lime-600 text-white p-3 rounded-lg hover:bg-lime-700 font-bold text-sm uppercase">
+                                            Ver candidatos
+                                        </a>
+                                    </div>
+                                </div>
+                                    
+                                @empty
+                                    <p class="text-center text-gray-600 uppercase p-3">No tienes notificaciones</p>
+
+                                    </p>
+                                @endforelse
+                                
+                       
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
